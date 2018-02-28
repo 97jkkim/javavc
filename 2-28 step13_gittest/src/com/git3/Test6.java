a7c01c8892943ced6771d1a35975c2d6eee24499
jun <97jkkim@naver.com>
2018-02-28 13:46:33 +0900

    3

--- 2-28 step13_gittest/src/com/git3/Test6.java
+++ 2-28 step13_gittest/src/com/git3/Test6.java
@@ -1,10 +1,10 @@
-package com.git2;
+package com.git3;
 
-public class Test4 {
+public class Test6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕 세상아아아~~");
+
 	}
 
 }
